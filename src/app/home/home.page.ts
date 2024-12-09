bf2bc70195e329bbcb1d0dc4392c4b076f84e033
Tobi <duartedx10@gmail.com>
2024-12-09 11:34:27 +0000

    paginas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  authMode: 'login' | 'signup' = 'login';
 
   constructor() {}
+
+  toggleAuthMode(mode: 'login' | 'signup') {
+    this.authMode = mode;
+  }
 
 }
